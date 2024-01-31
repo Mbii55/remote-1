88e204599211167fadfa69add73e5d3602bc130c
Mohamed Hassan <Mbii5567@hotmail.com>
2024-01-31 11:52:25 +0300

    Added random model year feature

--- cars.py
+++ cars.py
@@ -3,6 +3,10 @@ def cars():
     print('Toyota')
 
     cars_list = ['Toyota', 'Nissan', 'Dodge', 'GMC', 'BMW']
-    print(random.choice(cars_list))
+    return random.choice(cars_list)
 
-cars()
+def model():
+    models = ['2020', '2012', '2025', '2023', '2014', '2024', '2022']
+    return random.choice(models)
+
+print(f'car: {cars()} model: {model()}')
